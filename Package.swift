21b65f9abf143296c450e5160588985bd1147484
abhishek <abhishek@abhisheks-MacBook-Pro.local>
2024-02-20 20:06:27 +0530

    updated package.json

--- Package.swift
+++ Package.swift
@@ -1,51 +1,21 @@
-// swift-tools-version:5.3
-
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 import PackageDescription
 
 let package = Package(
-  name: "GoogleMobileAds",
-  platforms: [.iOS(.v12)],
+  name: "GoogleUserMessagingPlatform",
+  platforms: [.iOS(.v11)],
   products: [
     .library(
-      name: "GoogleMobileAds",
-      targets: ["GoogleMobileAdsTarget"]
-    )
-  ],
-  dependencies: [
-    .package(
       name: "GoogleUserMessagingPlatform",
-      url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git",
-      "1.1.0"..<"3.0.0"
+      targets: ["UserMessagingPlatform"]
     )
   ],
   targets: [
-    .target(
-      name: "GoogleMobileAdsTarget",
-      dependencies: [
-        .target(name: "GoogleMobileAds"),
-        .product(name: "GoogleUserMessagingPlatform", package: "GoogleUserMessagingPlatform"),
-      ],
-      path: "GoogleMobileAdsTarget"
-    ),
     .binaryTarget(
-      name: "GoogleMobileAds",
+      name: "UserMessagingPlatform",
       url:
-        "https://dl.google.com/googleadmobadssdk/175f5675c08ce9c3/googlemobileadsios-spm-11.0.1.zip",
-      checksum: "175f5675c08ce9c311341aa212748aa0600afb3dd6a86bab2fdff249f38c607a"
-    ),
+        "https://dl.google.com/googleadmobadssdk/f0f50864216a2469/googleusermessagingplatformios-spm-2.1.0.zip",
+      checksum: "f0f50864216a24690725f246df0b289d2eb6cc699b9da476857a0c135aeced84"
+    )
   ]
 )
