ad87cdb82a7ff6ea63ffd616f2ce758a87d1a91f
abhishek <abhishek@abhisheks-MacBook-Pro.local>
2024-02-20 20:13:45 +0530

    Added Appnexus name

--- Package.swift
+++ Package.swift
@@ -5,19 +5,19 @@
 import PackageDescription
 
 let package = Package(
-  name: "GoogleUserMessagingPlatform",
+  name: "AppNexusSDK",
   platforms: [.iOS(.v11)],
   products: [
     .library(
-      name: "GoogleUserMessagingPlatform",
-      targets: ["UserMessagingPlatform"]
+      name: "AppNexusSDK",
+      targets: ["AppNexusSDKTarget"]
     )
   ],
   targets: [
     .binaryTarget(
-      name: "UserMessagingPlatform",
+      name: "AppNexusSDKTarget",
       url:
-        "https://dl.google.com/googleadmobadssdk/f0f50864216a2469/googleusermessagingplatformios-spm-2.1.0.zip",
+        "https://acdn.adnxs.com/mobile/mtest/SwiftPM/AppNexusSDK.zip",
       checksum: "f0f50864216a24690725f246df0b289d2eb6cc699b9da476857a0c135aeced84"
     )
   ]
