43b031fbd5ab010a78fc20dc667c524307424fb3
abhishek <abhishek@abhisheks-MacBook-Pro.local>
2024-02-20 20:21:23 +0530

    Added checksum

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   products: [
     .library(
       name: "AppNexusSDK",
-      targets: ["AppNexusSDKTarget"]
+      targets: ["AppNexusSDKTarget","OMSDKTarget",]
     )
   ],
   targets: [
@@ -19,6 +19,12 @@ let package = Package(
       url:
         "https://acdn.adnxs.com/mobile/mtest/SwiftPM/AppNexusSDK.zip",
       checksum: "f83d9588b7e012da4ef5715e5ce48742d8f609fb553b755bc549b76ec982d3c8"
+    ),
+      .binaryTarget(
+      name: "OMSDKTarget",
+      url:
+        "https://acdn.adnxs.com/mobile/mtest/SwiftPM/OMSDK_Microsoft.zip",
+      checksum: "040551ebe0fc7d0f7c0619cae436105e6d4a263a1e3ff0257aa1f92e4c3d6952"
     )
   ]
 )
