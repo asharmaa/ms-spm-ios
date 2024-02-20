1cde95db1b515537b1af25da41378031ed4e6bf4
abhishek <abhishek@abhisheks-MacBook-Pro.local>
2024-02-20 20:14:33 +0530

    Added Appnexus name

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 
 let package = Package(
   name: "AppNexusSDK",
-  platforms: [.iOS(.v11)],
+  platforms: [.iOS(.v14)],
   products: [
     .library(
       name: "AppNexusSDK",
