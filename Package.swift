c29300917519a3148f5606a4786e33bea1db0f0f
abhishek <abhishek@abhisheks-MacBook-Pro.local>
2024-02-23 22:43:21 +0530

    Updated package

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     .binaryTarget(
       name: "AppNexusSDKTarget",
       url:
-        "file:///Users/abhishek/Documents/repo/sdk-legacy/8.11.0/dynamicframework/AppNexusSDK.xcframework.zip",
+        "https://github.com/asharmaa/ms-spm-ios/raw/master-2/AppNexusSDK.xcframework.zip",
       checksum: "8377924907206590c9087665da0bdf29d9551d11cbf738df009da94202553ba1"
     ),
       .binaryTarget(
       name: "OMSDKTarget",
       url:
-        "file:///Users/abhishek/Documents/repo/sdk-legacy/8.11.0/dynamicframework/OMSDK_Microsoft.zip",
+        "https://github.com/asharmaa/ms-spm-ios/raw/master-2/OMSDK_Microsoft.xcframework.zip",
       checksum: "7f598a65c54549c846e2ec1a63b10261666fad12b98c4c35de1b18ea3dcd3978"
     )
   ]
